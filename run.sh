a9321d78e19d2b18818d762106ae35a5896c2b05
Ronald Gouldner <gouldner@hawaii.edu>
2018-07-10 15:34:48 -1000

    added code to allow run on macbook for testing and tag id

--- run.sh
+++ run.sh
@@ -2,19 +2,29 @@
 
 DEBUG=$1
 
-#setup nvm since we run this script as a service
-export NVM_DIR="$HOME/.nvm"
-. ~/.nvmSetup
-#Note: This may be slow but it will select correct version of node
-nvm use v8.10.0
+RUNNINGON=`uname`
 
-BASEDIR=`dirname $0`
-if [ "${DEBUG}" == "" ]
+if [ "${RUNNINGON}" == "Darwin" ]
 then
-    # Quiet start
-    npm start index.js >> /dev/null 2>&1
-else
-    # Debug start
-    #npm start index.js >> ${BASEDIR}/room-assistant.log 2>&1
+    # on MacBook so just run
+    . ~/.nvm/nvm.sh
+    nvm use v8.10.0
     npm start index.js
+else
+    #setup nvm since we run this script as a service
+    export NVM_DIR="$HOME/.nvm"
+    . ~/.nvmSetup
+    #Note: This may be slow but it will select correct version of node
+    nvm use v8.10.0
+
+    BASEDIR=`dirname $0`
+    if [ "${DEBUG}" == "" ]
+    then
+        # Quiet start
+        npm start index.js >> /dev/null 2>&1
+    else
+        # Debug start
+        #npm start index.js >> ${BASEDIR}/room-assistant.log 2>&1
+        npm start index.js
+    fi
 fi
